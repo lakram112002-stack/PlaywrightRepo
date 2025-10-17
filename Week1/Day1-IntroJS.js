5d9cffb768f6203b961ecda82b6e5df25be3653c
lakram112002-stack <lakram112002@gmail.com>
2025-10-17 19:04:57 +0530

    Addint Week1 day 1 assignment

---
+++ Week1/Day1-IntroJS.js
@@ -0,0 +1,14 @@
+// Declare variables with different data types
+let userName = "Lak";             // String
+let organization = "ELM";      // String
+let contactNumber = 1234567890;     // Number
+let knowsAutomation = true;         // Boolean
+let usesPlaywright;                 // Undefined (not assigned)
+
+// Verify their types using typeof
+console.log(typeof userName);        // "string"
+console.log(typeof organization);    // "string"
+console.log(typeof contactNumber);   // "number"
+console.log(typeof knowsAutomation); // "boolean"
+console.log(typeof usesPlaywright);  // "undefined"
+
